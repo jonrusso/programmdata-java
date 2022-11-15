aa78ac0affbac332d0dbe5448c06527d26173c14
jonrusso <russovfx@icloud.com>
2022-11-15 17:23:27 -0300

    updatesondata

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+class ProgrammDataNumber01
+ {
+    public static void main (String entrada[])
+    {
+    //Declaration of variables
+    int NumInt;
+    double NumReal, addition;
+    char Caracter;
+
+    //Data input
+    NumInt = Integer.parseInt(entrada[0]);
+    NumReal = Double.parseDouble(entrada[1]);
+    Caracter = (entrada[2]) .charAt(0);
+    //proccess
+    addition = (double)NumInt + NumReal;
+    //output 
+    System.out.println((double)NumInt + "+" + NumReal + "=" + addition + "signal" + Caracter);
+
+    System.exit(0);
+    
     }
-}
+ }
